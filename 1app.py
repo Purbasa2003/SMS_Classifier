212cbd239b62ebd78fe4981d3f219c6873f64928
Purbasa2003 <purbasabhaduri07@gmail.com>
2024-07-20 11:35:35 +0530

    Add files via upload

---
+++ 1app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pickle
+
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+st.title("SMS Spam Classifier")
+
+input_sms = st.text_area("Enter the message")
+
+if st.button('Predict'):
+
+    transformed_sms = transform_text(input_sms)
+    vector_input = tfidf.transform([transformed_sms])
+    result = model.predict(vector_input)[0]
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
